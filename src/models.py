8e818dd943909fbd8a05b0b0a72bbb14c7e00108
Alvaro Ortiz Bustamante <Alvaro.Ortiz23@hotmail.com>
2019-05-14 20:11:43 +0000

    Diagrama UML

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,57 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Login(Base):
+    __tablename__ = 'Login'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    Nombre_Usuario = Column(String(15))
+    Password = Column(String(15))
+    Repassword = Column(String(15))
+    Estado_Cuenta = Column(Boolean, default=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'Usuario'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    Nickname = Column(String(15))
+    Correo = Column(String(15))
+    Telefono = Column(Integer)
+    Sexo = Column(Integer)
+    Estado_Cuenta = Column(Integer, ForeignKey('Login.Estado_Cuenta'))
+    Nombre_Usuario = Column(Integer, ForeignKey('Login.Nombre_Usuario'))
+    Password = Column(Integer, ForeignKey('Login.Password'))
+    Repassword = Column(Integer, ForeignKey('Login.Repassword'))
+    Login = relationship(Login)
+
+class Like(Base):
+    __tablename__ = 'Like'
+    id = Column(Integer, primary_key=True)
+    Cantidad_Like = Column(Integer)
+    Nombre_Usuario = Column(String(20))
+    Nickname = Column(String(20))
+    Seguido = Column(Boolean, default=True)
+
+class Publicacion(Base):
+    __tablename__ = 'Publicacion'
+    id = Column(Integer, primary_key=True)
+    Foto = Column(String(20))
+    Video = Column(String(20))
+    Texto = Column(String(20))
+    Comentario = Column(String(200))
+    Cantidad_Like = Column(Integer, ForeignKey('Like.Cantidad_Like'))
+    Like = relationship(Like)
+
+class Home(Base):
+    __tablename__ = 'Home'
+    id = Column(Integer, primary_key=True)
+    N_Publicaciones = Column(Integer)
+    N_Seguidos = Column(Integer)
+    Biografia = Column(String(100))
+    Publicacion_id = Column(Integer, ForeignKey('Publicacion.id'))
+    Publicacion = relationship(Publicacion)
+    Usuario_id = Column(Integer, ForeignKey('Usuario.id'))
+    Nickname = Column(Integer, ForeignKey('Usuario.Nickname'))
+    Nombre_Usuario = Column(Integer, ForeignKey('Usuario.Nombre_Usuario'))
+    Usuario = relationship(Usuario)
 
     def to_dict(self):
         return {}
